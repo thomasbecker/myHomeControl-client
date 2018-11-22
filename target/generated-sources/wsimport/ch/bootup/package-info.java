a64f842da92b1f905ba058258d84d17b02a79fff
Thomas Becker <thomas.becker00@gmail.com>
2018-11-22 18:05:24 +0100

    cleanup target

--- target/generated-sources/wsimport/ch/bootup/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * SOAP interface to access the various blocks of the model.
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bootup.ch/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ch.bootup;
